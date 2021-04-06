01f3b8b657980e116a212001ec15b3c046a6646f
ArnauPobladorNavarro-examen <arnautrv@gmail.com>
2021-04-06 12:52:22 +0200

    cambios wenos francis apruebanos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,13 +5,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../style/iniciarsesion.css">
-    <link rel="stylesheet" href="../links/bootstrap.min.css">
-    <link href="../FONTAWESOME/fontawesome-free-5.15.1-web/css/all.css" rel="stylesheet">
-    <script src="../links/bootstrap.bundle.min.js"></script>
-    <script src="../links/jquery-3.3.1.slim.min.js"></script>
-    <link rel="stylesheet" href="../style/navbarcss.css">
-    <script src="../js/navbarjs.js"></script>
+    <link rel="stylesheet" href="{{ asset('Cosasdelanterior/style/iniciarsesion.css') }}">
+    <link rel="stylesheet" href="{{ asset('Cosasdelanterior/links/bootstrap.min.css') }}">
+    <link href="{{ asset('Cosasdelanterior/FONTAWESOME/fontawesome-free-5.15.1-web') }}" rel="stylesheet">
+    <script src="{{ asset('Cosasdelanterior/links/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('Cosasdelanterior/links/jquery-3.3.1.slim.min.js') }}"></script>
+    <link rel="stylesheet" href="{{ asset('Cosasdelanterior/links/jquery-3.3.1.slim.min.js') }}">
+    <script src="{{ asset('Cosasdelanterior/js/navbarjs.js') }}"></script>
     <title>Iniciar Sesion</title>
 </head>
 
